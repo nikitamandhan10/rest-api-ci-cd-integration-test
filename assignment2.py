e6ace8ed57ce51d0e3dc34c25679c37494c45983
nikitamandhan10 <niki.mandhan10@gmail.com>
2023-10-04 21:12:03 -0700

    Assignment 2
    Health Check RESTful API /healthz

---
+++ assignment2.py
@@ -0,0 +1,27 @@
+from flask import Flask, Response
+import mysql.connector
+
+app = Flask(__name__)
+
+@app.route('/healthz', methods=['GET'])
+def check_db_connection():
+    try:
+        con = mysql.connector.connect(host='localhost', user= 'root', password = 'mysql123', auth_plugin='mysql_native_password')
+        return create_response(status=200)
+    except Exception as e:
+        return create_response(status=503)
+
+@app.route('/healthz', methods=['PUT', 'DELETE', 'PATCH', 'POST'])
+def other_methods():
+    return create_response(status=405)
+
+def create_response(status):
+    response = Response(status=status)
+    response.headers['Cache-control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=8080, debug=True)
