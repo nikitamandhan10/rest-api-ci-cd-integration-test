27a9ec06982209194dbd0cb993063654c63fffc9
nikitamandhan10 <niki.mandhan10@gmail.com>
2023-10-05 20:30:43 -0700

    Testing mysql

--- assignment2.py
+++ assignment2.py
@@ -7,9 +7,10 @@ print('Initializing app')
 @app.route('/healthz', methods=['GET'])
 def check_db_connection():
     try:
-        con = mysql.connector.connect(host='localhost', user= 'root', password = 'root', auth_plugin='mysql_native_password')
+        con = mysql.connector.connect(host='localhost', user= 'root', password = 'root')
         return create_response(status=200)
     except Exception as e:
+        print("Exception: ", e)
         return create_response(status=503)
 
 @app.route('/healthz', methods=['PUT', 'DELETE', 'PATCH', 'POST'])
